5668f46828bf36200334272778e9fc92cae73577
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-07-29 01:49:47 +0630

    first commit

---
+++ Sample_Payment2K24/Modules/LoginScreenModule/LoginScreenViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginScreenViewController.swift
+//  Sample_Payment2K24
+//
+//  Created by Aung Kyaw Phyo on 29/07/2024.
+//
+
+import UIKit
+
+class LoginScreenViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailErrorLabel: UILabel!
+    @IBOutlet weak var passwordErrorLabel: UILabel!
+    @IBOutlet weak var googleBtn: UIButton!
+    @IBOutlet weak var facebookBtn: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
