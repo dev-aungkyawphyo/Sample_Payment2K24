c772143f1e21cf4d1e990bb3e55b6a9ad9a286b7
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-07-31 02:14:14 +0630

    Update port

--- Sample_Payment2K24/Modules/DesignableTabBarScreenModule/DesignableTabBarScreenViewController.swift
+++ Sample_Payment2K24/Modules/DesignableTabBarScreenModule/DesignableTabBarScreenViewController.swift
@@ -11,10 +11,47 @@ class DesignableTabBarScreenViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        defaultSetup()
     }
     
-
+    private func defaultSetup() {
+        setupRadius()
+    }
     
+    private func setupRadius() {
+        self.tabBar.layer.masksToBounds = true
+        self.tabBar.isTranslucent = true
+        self.tabBar.layer.cornerRadius = 20
+        self.tabBar.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        configHegiht()
+    }
+    
+    private func configHegiht() {
+        if UIDevice().userInterfaceIdiom == .phone {
+            var tabFrame = tabBar.frame
+            tabFrame.size.height = 82
+            tabFrame.origin.y = view.frame.size.height - 82
+            tabBar.frame = tabFrame
+        }
+    }
+    
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        animationWhenSelectItem(item)
+    }
 
+    func animationWhenSelectItem(_ item: UITabBarItem){
+        guard let barItemView = item.value(forKey: "view") as? UIView else {return}
+        let timeInterval: TimeInterval = 0.3
+        let propertyAnimator = UIViewPropertyAnimator(duration: timeInterval, dampingRatio: 0.5) {
+            barItemView.transform = CGAffineTransform.identity.scaledBy(x: 0.8, y: 0.8)
+        }
+        propertyAnimator.addAnimations({ barItemView.transform = .identity },
+                                       delayFactor: CGFloat(timeInterval))
+        propertyAnimator.startAnimation()
+    }
+  
 }
