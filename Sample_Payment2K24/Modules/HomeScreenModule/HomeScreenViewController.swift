7bf2e1bdb7de635bbc2feb9da31e7b84c24e2b51
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2024-08-01 04:50:54 +0630

    upload commit

--- Sample_Payment2K24/Modules/HomeScreenModule/HomeScreenViewController.swift
+++ Sample_Payment2K24/Modules/HomeScreenModule/HomeScreenViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class HomeScreenViewController: UIViewController {
+    
+    // MARK: Outlets
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var userAmountLabel: UILabel!
+    @IBOutlet weak var posterCollectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var historyTable: UITableView!
 
+    // MARK: Lifecycles
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-  // TODO: HOME SCREEN 
+  
 }
 
--- Sample_Payment2K24/Modules/RewardScreenModule/RewardScreenViewController.swift
+++ Sample_Payment2K24/Modules/RewardScreenModule/RewardScreenViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class NotificationScreenViewController: UIViewController {
+class RewardScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
